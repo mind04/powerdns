43a43946aa4b2a5ff3c8257413b6a29c37e84cf7
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-03-12 14:00:43 +0100

    add pair test

--- pdns/testci.cc
+++ pdns/testci.cc
@@ -1,4 +1,7 @@
 #include "misc.hh"
+#include <utility>
+
+typedef pair<string, uint16_t> typedns_t;
 
 int main(int argc, char **argv)
 {
@@ -8,5 +11,15 @@ int main(int argc, char **argv)
 	nsset.insert("");
 	for(set<string, CIStringCompare>::const_iterator i=nsset.begin();i!=nsset.end();++i) {
 		cout<<"["<<*i<<"]"<<endl;
+	}
+
+	set<typedns_t, CIStringPairCompare> nsset2;  
+	nsset2.insert(make_pair("ns.example.com", 1));
+	nsset2.insert(make_pair("abc", 1));
+	nsset2.insert(make_pair("", 1));
+	nsset2.insert(make_pair("abc", 2));
+	nsset2.insert(make_pair("ns.example.com", 0));
+	for(set<typedns_t, CIStringPairCompare>::const_iterator i=nsset2.begin();i!=nsset2.end();++i) {
+		cout<<"["<<i->first<<"]["<<i->second<<"]"<<endl;
 	}
 }
